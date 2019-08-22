af9d68a102a05099e072a0c1ed5603e890a92949
maycon <mayconramalhopereira@outlook.com.br>
2019-08-21 23:47:45 -0300

    commit

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import reactDom from 'react-dom'
 
-const elemento = <h2>Deu cert</h2>
+const elemento = <h2>Hello World</h2>
 
 // semelhante a $('<h1>').html('React 2')
 reactDom.render(elemento, document.getElementById('root'))
